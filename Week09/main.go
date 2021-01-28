3e6aabed152b716fa4f13f3578afcc2043930c86
墨 <gray_xiaoniu@163.com>
2021-01-28 22:26:03 +0800

    作业 week-09

--- Week09/main.go
+++ Week09/main.go
@@ -1,11 +1,3 @@
-学习笔记
-G20200607011074
-> 1. 用 Go 实现一个 tcp server ，用两个 goroutine 读写 conn，两个 goroutine 通过 chan 可以传递 message，能够正确退出
-以上作业，要求提交到 GitHub 上面，Week09 作业地址：
-https://github.com/Go-000/Go-000/issues/82
-
-> 代码测试有问题，只返回了一次就会断开，为什么？？？
-```golang
 package main
 
 import (
@@ -99,4 +91,3 @@ func connWriter(ctx context.Context, conn net.Conn, msg chan string) {
 // 		writer.Flush()
 // 	}
 // }
-```
